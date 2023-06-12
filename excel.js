23c7b430374dd0ec91c5bd336dd7fadebc9d745b
yo-young2 <134292561+yo-young2@users.noreply.github.com>
2023-06-12 19:30:48 +0900

    v 1.1

--- excel.js
+++ excel.js
@@ -1,103 +1,8 @@
-const http = require('http');
-const express = require('express');
-const app = express();
-const port = 3000;
-const fs = require('fs');
-const folder = './list/';
-
-app.use(express.static('public'));
-app.set('view engine', 'jade');
-app.set('views', './views'); 
-app.get('/', function(req, res){
-  var template = `
-  <!doctype html>
-  <html>
-    <head>
-      <title>FILELIST</title>
-      <meta charset = "utf-8">
-    </head>
-    <body>
-      <h1>엑셀변환 매크로</h1>
-      <a href="/upload">엑셀 변환하기</a>
-      <br>
-      <a href="/list">엑셀변환 완료목록</a>
-      </body>
-  </html>`;
-  res.end(template);
-});
-app.listen(port,function(){
-  console.log(`Example app listening on port ${port}`)
-});
-const bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({extended: false}));
-app.locals.pretty = true;
 const multer = require('multer');
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, 'order/');
-  },
-  filename: function (req, file, cb) {
-    cb(null, 'input.xlsx');
-  }
-});
-const upload = multer({ storage: storage });
-//const upload = multer({ dest: 'uploads/'})
+const fs = require('fs');
 const multiparty = require('multiparty');
 const xlsx = require('xlsx');
-
-app.get('/list',function(req,res){
-  fs.readdir(folder, function(err, filelist){
-    var list = '<ul>'
-    var i =0;
-    while(i<filelist.length){
-      list = list + `<li><a href="./list/${filelist[i]}" download>${filelist[i]}</a></li>`;
-      i = i+1;
-    }
-    list = list + '<ul>'
-    var template = `
-    <!doctype html>
-    <html>
-      <head>
-        <title>FILELIST</title>
-        <meta charset = "utf-8">
-      </head>
-      <body>
-        <h1>완료 목록</h1>
-        ${list}
-      </body>
-    </html>`;
-    res.end(template);
-  })
-});
-app.get('/upload',function(req,res){
-    res.render('upload');
-});
-app.post('/upload',  upload.single('userfile'), function(req,res){
-  excelsort();
-  fs.readdir(folder, function(err, filelist){
-    var list = '<ul>'
-    var i =0;
-    while(i<filelist.length){
-      list = list + `<li><a href="https://port-0-wave-00-dihik2mliiebw85.sel4.cloudtype.app/list/${filelist[i]}" download>${filelist[i]}</a></li>`;
-      i = i+1;
-    }
-    list = list + '<ul>'
-    var template = `
-    <!doctype html>
-    <html>
-      <head>
-        <title>FILELIST</title>
-        <meta charset = "utf-8">
-      </head>
-      <body>
-        <h1>완료 목록</h1>
-        ${list}
-      </body>
-    </html>`;
-    res.end(template);
-  })
-});
-
+const folder = './list/';
 
 function excelsort(){
 const excelFile = xlsx.readFile("order/input.xlsx");
@@ -109,7 +14,6 @@ const jsonData = xlsx.utils.sheet_to_json(sheetData, {range: 0, defval : ""});
 let checksum = 1000;
 let result = new Array();
 const store_excel = xlsx.utils.book_new();
-
 
 for(let i=0;i<j+1;i++){
         let order_number = jsonData[i].수량;
@@ -149,4 +53,8 @@ var FileDate = Year+month+day;
 var FilePath = './list/OUTPUT_'+FileDate+'.xlsx';
 xlsx.utils.book_append_sheet(store_excel,outJson);
 xlsx.writeFile(store_excel,FilePath);
-} 
+};
+
+module.exports = {
+  excelsort
+}
