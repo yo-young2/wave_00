6117aa86387002abbfc2b02f274709eab34c7e90
yo-young2 <134292561+yo-young2@users.noreply.github.com>
2023-07-28 17:28:43 +0900

    v 2.0.02

--- routes/home-routes.js
+++ routes/home-routes.js
@@ -1,13 +1,18 @@
-const {homeview, generatePdf,uploadview, LJuploadview, listsview}  = require('../controllers/homeController');
-const {dodam_print_view, modifyPdf_dodam}  = require('../labelforms/dodam_label');
+const {homeview,generate_pdf,uploadview,ljuploadview,listsview} = require('../controllers/homeController');
+// import listsview from "listsview";
+// import LJuploadview from "LJuploadview"
+// import uploadview from "uploadview";
+// import generatePdf from "generatePdf";
+// import homeview from "homeview";
+const {dodam_print_view, modifyPdf_dodam} = require('../labelforms/dodam_label');
 const express = require('express');
 const router = express.Router();
 
 router.get('/', homeview);
 router.get('/upload', uploadview);
-router.get('/upload_LJ', LJuploadview);
+router.get('/upload_LJ', ljuploadview);
 router.get('/list', listsview);
-router.get('/download', generatePdf);
+router.get('/download', generate_pdf);
 router.get('/dodam_printlist', dodam_print_view);
 router.get('/dodam_print', modifyPdf_dodam);
 
